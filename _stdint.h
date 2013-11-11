748c8ccf6985d15f3fbe39c41dcf96271e96e5a7
Greg Nietsky <gregory@distrotech.co.za>
2013-11-11 14:01:51 +0200

    Clean

--- _stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GST_PLUGINS_UGLY__STDINT_H
-#define _GST_PLUGINS_UGLY__STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-ugly 1.0.9"
-/* generated using gnu compiler gcc (GCC) 4.8.1 20130530 (prerelease) */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
